947f3a38b290ad5614f9bae36c35a1604739afb0
“[firstname <emmanuelajah188@gmail.com>
2023-05-19 15:00:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+  echo("Hello Php");
+  echo '<br>';
+  $Nme = '<h2>emmnuel</h2>';
+
+  Echo $Nme;
+
+  $firstNme = 'emmnuel';
+
+  define("publisher", "STYLES");
+  echo publisher;
+
+  // Types In php 
+// interger 
+// floting-Point Numbers 
+// Strings 
+// Boolens
+// RRY
+// Objects 
+// Resource 
+// Cllbck
+// Null 
+
+$speed = -2355;
+$pInt = 203;
+$Hex = 0xFFF;
+$hex = -0xDD2;
+
+
+$flot = 3.47;
+$consnt = 23.0E-3; // 23.0*20**(-3)
+
+
+$strings = 'Hi i\'m Big Boy';
+
+echo 'Hello' . $strings;
+
+$Bool = true;
+$Booldog = false;
+
+if($Bool === true){
+  echo '<br>';
+   echo 'I am true';
+}else{
+  echo 'I am false';
+}
+
+{
+  // $varible = 'Joshua';
+  define('Variable','Josha'); 
+}
+
+$friends = 'Mick';
+$friends = 'Ogechi';
+echo $friends;
+
+
+$Foods = ['Rice', 'Yam', 'Beans', 'Soup'];
+echo '<br>';
+var_dump($Foods);
+
+$Books = []; //Empty array
+
+$Books[0] = 'Emekos';
+$Books[2] = 'Sunday';
+$Books[3] = 'abigail';
+$Books[4] = 'Chinonso';
+$Books[5] = 'joy';
+echo '<br>';
+echo '<br>';
+
+$Books['Emekos'];
+
+var_dump($Books);
+
+echo '<br>';
+echo '<br>';
+
+$person = array("name" => 'Philip');
+
+var_dump($person);
+
+
+
+
+
+
+
+
+  
+
+ 
